c83ee1a63971f872cc6396d369668c097b080595
mithelankd <mithelan@betalaunch.io>
2023-01-23 11:58:45 +0530

    sample build

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: 'https://metjip-dev.s3.eu-central-1.amazonaws.com/organization/634642f3b43047b47d90faca/documents/634642f4b43047b47d90fad9?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAYWVMBN7NGLYV7DEA%2F20230123%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230123T060850Z&X-Amz-Expires=2700&X-Amz-Signature=e080b7eeccd6605f08f97b9d63c66fbf36763e1c9a966a2b9e0dbc65034b780d&X-Amz-SignedHeaders=host',
       },
       viewer.current,
     ).then((instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
-
+      const UIEvents = instance.UI.Events;
+      instance.UI.addEventListener(UIEvents.LOAD_ERROR, (error) => {
+        console.log("error: ", error);
+        
+      });
       documentViewer.addEventListener('documentLoaded', () => {
         const rectangleAnnot = new Annotations.RectangleAnnotation({
           PageNumber: 1,
